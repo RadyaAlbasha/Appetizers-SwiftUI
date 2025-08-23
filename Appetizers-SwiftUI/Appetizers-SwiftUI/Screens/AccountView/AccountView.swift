f89c292c810bd5797bbf2d30c5fc047546dd0f93
RadyaAlbasha <radya.albasha15@gmail.com>
2025-08-23 17:10:38 +0300

    Add account view

---
+++ Appetizers-SwiftUI/Appetizers-SwiftUI/Screens/AccountView/AccountView.swift
@@ -0,0 +1,21 @@
+//
+//  AccountView.swift
+//  Appetizers-SwiftUI
+//
+//  Created by Radya Albasha on 13/06/2025.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        NavigationStack{
+            Text("Account View")
+                .navigationTitle("😃   Account")
+        }
+    }
+}
+
+#Preview {
+    AccountView()
+}
