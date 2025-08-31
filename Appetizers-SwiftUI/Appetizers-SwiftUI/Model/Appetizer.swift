98e93a123e8ed8655a9d42a9d5c71fa28c5e91df
RadyaAlbasha <radya.albasha15@gmail.com>
2025-08-31 21:17:05 +0300

    create order view and delete from list

--- Appetizers-SwiftUI/Appetizers-SwiftUI/Model/Appetizer.swift
+++ Appetizers-SwiftUI/Appetizers-SwiftUI/Model/Appetizer.swift
@@ -21,7 +21,7 @@ struct AppetizerResponse: Decodable {
 }
 
 struct MockData {
-    static let sampleAppetizer: Appetizer = Appetizer(id: 1,
+    static let sampleAppetizer: Appetizer = Appetizer(id: 0001,
                   name: "Crispy Calamari",
                   description: "Lightly breaded and fried calamari rings.",
                   price: 12.99,
@@ -30,4 +30,32 @@ struct MockData {
                   protein: 12,
                   carbs: 30)
     static let appetizers: [Appetizer] = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+    
+    static let orderItemOne: Appetizer = Appetizer(id: 0001,
+                  name: "Crispy Calamari one",
+                  description: "Lightly breaded and fried calamari rings.",
+                  price: 12.99,
+                  imageURL: "",
+                  calories: 250,
+                  protein: 12,
+                  carbs: 30)
+    
+    static let orderItemTwo: Appetizer = Appetizer(id: 0002,
+                  name: "Crispy Calamari two",
+                  description: "Lightly breaded and fried calamari rings.",
+                  price: 12.99,
+                  imageURL: "",
+                  calories: 250,
+                  protein: 12,
+                  carbs: 30)
+    
+    static let orderItemThree: Appetizer = Appetizer(id: 0003,
+                  name: "Crispy Calamari three",
+                  description: "Lightly breaded and fried calamari rings.",
+                  price: 12.99,
+                  imageURL: "",
+                  calories: 250,
+                  protein: 12,
+                  carbs: 30)
+    static let orderItems: [Appetizer] = [orderItemOne, orderItemTwo, orderItemThree] 
 }
--- Appetizers-SwiftUI/Appetizers-SwiftUI/Screens/OrderView/OrderView.swift
+++ Appetizers-SwiftUI/Appetizers-SwiftUI/Screens/OrderView/OrderView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct OrderView: View {
+    @State private var orderItems = MockData.orderItems
     var body: some View {
         NavigationStack{
-            Text("Order View")
-                .navigationTitle("🧾 Orders")
+            VStack{
+                List{
+                    ForEach(orderItems){ Appetizer in
+                        AppetrizerListCell(appetizer: Appetizer)
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .listStyle(.plain)
+                
+                Button {
+                    print("Place Order")
+                } label: {
+                    APButton(title: "$99.99 Place Order")
+                }
+                .padding(.bottom, 25)
+            }
+            .navigationTitle("🧾 Orders")
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
     }
 }
 
